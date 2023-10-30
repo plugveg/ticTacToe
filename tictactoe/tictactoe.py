330d32e0155ac37420044d7b4db81e24b1b0ade7
Kirite11111 <eliott.garnier2810@gmail.com>
2023-10-30 14:04:25 +0100

    ajout fonction winner

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -81,6 +81,20 @@ def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
+    for i in range(3):
+        # Check rows and columns
+        if board[i][0] == board[i][1] == board[i][2] and board[i][0] is not None:
+            return board[i][0]
+        if board[0][i] == board[1][i] == board[2][i] and board[0][i] is not None:
+            return board[0][i]
+
+    # Check diagonals
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] is not None:
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] and board[0][2] is not None:
+        return board[0][2]
+
+    return None
     raise NotImplementedError
 
 
